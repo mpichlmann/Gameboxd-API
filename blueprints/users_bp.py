5aa920b8e420f4b537560a3d528029c23bef83d1
mpichlmann <14205@coderacademy.edu.au>
2023-06-22 19:44:50 +1000

    added ability for admins to make other users admins

--- blueprints/users_bp.py
+++ blueprints/users_bp.py
@@ -1,6 +1,8 @@
 from init import db
-from flask import Blueprint
+from flask import Blueprint, request
 from models.user import User, UserSchema
+from flask_jwt_extended import jwt_required
+from blueprints.auth_bp import admin_required
 
 users_bp = Blueprint('users', __name__, url_prefix='/users')
 
@@ -16,7 +18,17 @@ def all_users():
 
 
 # Update a user - make admin - only admins can do this 
-
+@users_bp.route('/<int:user_id>', methods=['PUT', 'PATCH'])
+@jwt_required()
+def make_admin(user_id):
+    user = User.query.get(user_id)
+    if user:
+        admin_required()
+        user.is_admin = True
+        db.session.commit()
+        return {'message': f'user with ID {user.id} is now an admin'}
+    else:
+        return {'error': 'User not found'}, 404
 
 # Delete a user 
 
